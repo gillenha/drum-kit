1cf2c18c75e4b23ff89ba89d7942493c15e4e430
gillenha <gillen08@gmail.com>
2019-01-28 01:46:37 -0500

    Updates

---
+++ precache-manifest.d6bb45416086c988ab3a82b0ed1f510a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dd585951cfd8c9f78cf3241ec4bbb7d",
+    "url": "/drum-kit/static/media/kick.4dd58595.wav"
+  },
+  {
+    "revision": "4056ebda2b00070b0f97",
+    "url": "/drum-kit/static/css/main.e81973f2.chunk.css"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/drum-kit/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "4e92e049e52e13f400fe",
+    "url": "/drum-kit/static/js/runtime~main.4e92e049.js"
+  },
+  {
+    "revision": "550be808a1e25f5b4db6b66350270316",
+    "url": "/drum-kit/static/media/boom.550be808.wav"
+  },
+  {
+    "revision": "a6e86dd7c2d3dee1ec0ad70c0d69df36",
+    "url": "/drum-kit/static/media/clap.a6e86dd7.wav"
+  },
+  {
+    "revision": "4ba39e07d1f8d98b362b9b65d6544538",
+    "url": "/drum-kit/static/media/hihat.4ba39e07.wav"
+  },
+  {
+    "revision": "4056ebda2b00070b0f97",
+    "url": "/drum-kit/static/js/main.4056ebda.chunk.js"
+  },
+  {
+    "revision": "a62db465466a58a60a40c4466d7730e5",
+    "url": "/drum-kit/static/media/openhat.a62db465.wav"
+  },
+  {
+    "revision": "b0d70ec54db9b098b9358f98d131c0f9",
+    "url": "/drum-kit/static/media/ride.b0d70ec5.wav"
+  },
+  {
+    "revision": "80c4129b4b6b8810a079d6c311bf1d4b",
+    "url": "/drum-kit/static/media/snare.80c4129b.wav"
+  },
+  {
+    "revision": "2a6bb6f58bb711ed7ea121b225dcc92d",
+    "url": "/drum-kit/static/media/tink.2a6bb6f5.wav"
+  },
+  {
+    "revision": "572ed39d3a51fff664d47d7b7b508292",
+    "url": "/drum-kit/static/media/tom.572ed39d.wav"
+  },
+  {
+    "revision": "ef516c0944256ebb5136cd1c67515459",
+    "url": "/drum-kit/static/media/drummer.ef516c09.jpg"
+  },
+  {
+    "revision": "5fc490ec9e8edf417eb063626985b6d0",
+    "url": "/drum-kit/index.html"
+  }
+];
